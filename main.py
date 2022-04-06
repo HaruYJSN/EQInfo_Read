514cb247b40dee73a380928a7f540ef5d8368644
HaruYJSN <haumume@gmail.com>
2022-04-06 18:23:20 +0900

    test

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import wave
+from time import sleep
 #変数定義
 api = "https://api.p2pquake.net/v2/history?limit=1"
 eqinfo = "&codes=551"
@@ -13,7 +15,8 @@ eqdatan = tsunamidatan = dict
 def get_latest_data(data):
     raw = requests.get(api+data)
     # print(type(raw))
-    return json.loads(raw.text)
+    text=raw.text
+    return json.loads(text)
 
 # def latest_json(data):
 #     return json.load(get_latest_data(data))
@@ -40,7 +43,7 @@ def shindostr(value):
     elif value == 70:
         return "震度7"
     elif value == -1:
-        return 
+        return "震度の情報はありません"
 
 def parsejson_eq(data):
     # print(type(data))
@@ -57,11 +60,37 @@ def parsejson_eq(data):
         
     maxshindo = info['maxScale']
     tsunami = info['domesticTsunami']
+    shindo70 = shiindo65 = shindo60 = shindo50 = shindo45 = shindo40 = shindo30 = shindo20 = shindo10 = ""
+
+    kakuchi = eq['points']
+    # 各地の震度情報
+    for i in range(0,len(kakuchi)):
+        if kakuchi[i]['scale'] == "70":
+            shindo70 = shindo70 + kakuchi[i]['pref'] + kakuchi[i]['addr']
+        elif kakuchi[i]['scale'] == "65":
+            shindo65 = shindo65 + kakuchi[i]['pref'] + kakuchi[i]['addr']
+        elif kakuchi[i]['scale'] == "60":
+            shindo60 = shindo60 + kakuchi[i]['pref'] + kakuchi[i]['addr']
+        elif kakuchi[i]['scale'] == "50":
+            shindo50 = shindo50 + kakuchi[i]['pref'] + kakuchi[i]['addr']
+        elif kakuchi[i]['scale'] == "45":
+            shindo45 = shindo45 + kakuchi[i]['pref'] + kakuchi[i]['addr']
+        elif kakuchi[i]['scale'] == "40":
+            shindo40 = shindo40 + kakuchi[i]['pref'] + kakuchi[i]['addr']
+        elif kakuchi[i]['scale'] == "30":
+            shindo30 = shindo30 + kakuchi[i]['pref'] + kakuchi[i]['addr']
+        elif kakuchi[i]['scale'] == "20":
+            shindo20 = shindo20 + kakuchi[i]['pref'] + kakuchi[i]['addr']
+        elif kakuchi[i]['scale'] == "10":
+            shindo10 = shindo10 + kakuchi[i]['pref'] + kakuchi[i]['addr']
+
+    #print(eq['points'])
     # 電文作成
     if jouhou == "ScalePrompt":
         jouhoustr = "震度速報"
     elif jouhou == "Destination":
         jouhoustr = "震源に関する情報"
+        return
     elif jouhou == "ScaleAndDestination":
         jouhoustr = "震源･震度に関する情報"
     elif jouhou == "DetailScale":
@@ -70,6 +99,7 @@ def parsejson_eq(data):
         jouhoustr = "遠地地震に関する情報"
     elif jouhou == "Other":
         jouhoustr = "その他の情報"
+        return
 
 
     if tsunami == "None":
@@ -84,7 +114,11 @@ def parsejson_eq(data):
         tsunamistr = "津波注意報が発表されています.今後の情報に警戒してください."
     elif tsunami == "Warning":
         tsunamistr = "津波予報が発表されています.今後の情報に警戒してください."
-    print(jouhoustr+"を受信しました.\n"+timestr+",最大震度"+shindostr(maxshindo)+"を観測する地震がありました.\n"+tsunamistr+"\n")
+    denbun1 = jouhoustr+"を受信しました.\n"+timestr+",最大"+shindostr(maxshindo)+"を観測する地震がありました.\n"+tsunamistr+"\n震源は"+hyponame+",深さ"+str(depth)+"km.地震の規模を示すマグニチュードは,"+str(magnitude)+"です."
+    denbun2 = "各地の震度をお伝えします.\n"
+    
+    print(denbun1)
+    generate_wav(denbun1)
 
     print()
 
@@ -98,7 +132,7 @@ def datacheck():
     eqdatao = eqdatan
     eqdatan = get_latest_data(eqinfo)
     tsunamidatao = tsunamidatan
-    tsunamidatan = get_latest_data(tsunamiinfo)
+    tsunamidatan = None#get_latest_data(tsunamiinfo)
 
     # 新旧比較
     # 0=更新なし 1=更新あり
@@ -110,12 +144,37 @@ def datacheck():
     if tsunamidatao == tsunamidatan:
         tsunamiupdate = 0
     else:
-        tsunamiupdate = 1
+        tsunamiupdate = 0
     
 
 def gentalk(text):
     None
 
+def generate_wav(text, speaker=8, filepath='./audio.wav'):
+    host = 'localhost'
+    port = 50021
+    params = (
+        ('text', text),
+        ('speaker', speaker),
+    )
+    response1 = requests.post(
+        f'http://{host}:{port}/audio_query',
+        params=params
+    )
+    headers = {'Content-Type': 'application/json',}
+    response2 = requests.post(
+        f'http://{host}:{port}/synthesis',
+        headers=headers,
+        params=params,
+        data=json.dumps(response1.json())
+    )
+
+    wf = wave.open(filepath, 'wb')
+    wf.setnchannels(1)
+    wf.setsampwidth(2)
+    wf.setframerate(24000)
+    wf.writeframes(response2.content)
+    wf.close()
 
 # mainloop
 while 1:
@@ -124,5 +183,5 @@ while 1:
         parsejson_eq(eqdatan)
     if tsunamiupdate==1:
         parsejson_tsunami(tsunamidatan)
-
+    sleep(4)
 
